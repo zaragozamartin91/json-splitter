b2c2ce2423d12fdc97ff63c7bbbce18239134267
zaragozamartin91 <zaragozamartin91@gmail.com>
2026-01-10 21:52:41 +0100

    Removed usage of newer java feature

--- src/main/java/io/github/zaragozamartin91/splitter/SplitJson.java
+++ src/main/java/io/github/zaragozamartin91/splitter/SplitJson.java
@@ -1,14 +1,14 @@
 package io.github.zaragozamartin91.splitter;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SplitJson {
     private final List<FlatJson> parts;
 
     public SplitJson(Collection<FlatJson> parts) {
-        this.parts = List.copyOf(parts);
+        this.parts = new ArrayList<>(parts);
     }
 
     public FlatJson getPart(int index) {
